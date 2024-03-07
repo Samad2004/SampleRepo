0040d8f3ec58e80340f00ffe17b7e137223886bd
Samad2004 <mohdsamadshaik@gmail.com>
2024-03-07 22:06:52 +0530

    new work

---
+++ sw.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class sw{
+
+    public static void main(String [] args){
+
+        byte ramm=16;
+        short ball=45;
+        int age=18;
+        long num=12390797;
+        float dec=3.147299f;
+        double deci=456.098765432;
+        char boy="B";
+        bool stat= TRUE;
+
+        System.out.println("Hello World");
+        System.out.println("byte s",ramm);
+        System.out.println("short s",ball);
+        System.out.println("int s",age);
+        System.out.println("long s",num);
+        System.out.println("float s",dec);
+        System.out.println("double s",deci);
+        System.out.println("char s",boy);
+        System.out.println("bool s",stat);
+    }
+}
